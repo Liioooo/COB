4c45c05a503f160f10bdfc8dc2c074eb06ced719
Leo Preissegger <preissegger.leo@a1.net>
2019-03-12 17:01:17 +0100

    [TASK] added basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
