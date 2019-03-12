1439b638ead59c5ac8324d10e4b9173c364856a0
Leo Preissegger <preissegger.leo@a1.net>
2019-03-12 17:33:55 +0100

    added drag and drop

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
     exports: [
-        MatButtonModule
+        MatButtonModule,
+        DragDropModule
     ]
 })
 export class MaterialModule { }
