be51b50ae02199bea6448141c7f4efd584c7d55b
Leo Preissegger <preissegger.leo@a1.net>
2019-03-13 08:57:33 +0100

    removed ng-sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
-import { SidebarModule } from 'ng-sidebar';
 import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
 import {SmallPagePreviewComponent} from './components/small-page-preview/small-page-preview.component';
 
@@ -20,7 +19,6 @@ import {SmallPagePreviewComponent} from './components/small-page-preview/small-p
   ],
   imports: [
     BrowserModule,
-    SidebarModule.forRoot(),
     BrowserAnimationsModule,
     MaterialModule
   ],
