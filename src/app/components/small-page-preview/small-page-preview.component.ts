c3702ce36b1f02ce305b6a6f6c686aadbe2369fd
Liiioooo <preissegger.leo@a1.net>
2019-03-15 18:33:08 +0100

    [FIX] selection get changed on drag

--- src/app/components/small-page-preview/small-page-preview.component.ts
+++ src/app/components/small-page-preview/small-page-preview.component.ts
@@ -24,20 +24,35 @@ export class SmallPagePreviewComponent implements OnInit {
     @Input()
     page: Page;
 
+    private startMousePosX: number;
+    private startMousePosY: number;
+
     constructor(private pageStructure: PageStructureService) { }
 
     ngOnInit() {
     }
 
-    onClick(event: MouseEvent) {
-        if (event.ctrlKey) {
-          this.pageStructure.switchSelection(this.page);
-        } else {
-          if (this.pageStructure.selectedPages.length === 1 && this.page.isSelected) {
-            this.pageStructure.switchSelection(this.page);
-          } else {
-            this.pageStructure.selectedPages = [this.page];
-          }
+    mouseDown(event: MouseEvent) {
+        this.startMousePosX = event.clientX;
+        this.startMousePosY = event.clientY;
+    }
+
+    mouseUp(event: MouseEvent) {
+        if (this.distance(this.startMousePosX, this.startMousePosY, event.clientX, event.clientY) > 5) {
+            return;
         }
+        if (event.ctrlKey) {
+            this.pageStructure.switchSelection(this.page);
+        } else {
+            if (this.pageStructure.selectedPages.length === 1 && this.page.isSelected) {
+                this.pageStructure.switchSelection(this.page);
+            } else {
+                this.pageStructure.selectedPages = [this.page];
+            }
+        }
+    }
+
+    private distance(x1: number, y1: number, x2: number,  y2: number): number {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
