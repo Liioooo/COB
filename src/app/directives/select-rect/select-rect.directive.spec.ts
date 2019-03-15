73c7d36d0408b50e4ff42b4dd99f8da2f01c0233
Liiioooo <preissegger.leo@a1.net>
2019-03-15 18:03:35 +0100

    draw select rect

---
+++ src/app/directives/select-rect/select-rect.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SelectRectDirective } from './select-rect.directive';
+
+describe('SelectRectDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SelectRectDirective();
+    expect(directive).toBeTruthy();
+  });
+});
