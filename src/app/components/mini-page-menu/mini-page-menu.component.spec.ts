0372811173c210a0c1fbef478efd410e25f4b3c0
Alex Mimü <6150@htl.rennweg.at>
2019-03-19 17:20:02 +0100

    [TASK, FIX] if a page is selected 3 buttons (delete, copy, cut) appear at the side of the page; ctrl-d deselects selection; dragged pages don't collide with itself

---
+++ src/app/components/mini-page-menu/mini-page-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MiniPageMenuComponent } from './mini-page-menu.component';
+
+describe('MiniPageMenuComponent', () => {
+  let component: MiniPageMenuComponent;
+  let fixture: ComponentFixture<MiniPageMenuComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MiniPageMenuComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MiniPageMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/mini-page-menu/mini-page-menu.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {PageStructureService} from '../../services/PageStructure/page-structure.service';
+import {Page} from '../../models/page-interface';
+
+@Component({
+  selector: 'app-mini-page-menu',
+  templateUrl: './mini-page-menu.component.html',
+  styleUrls: ['./mini-page-menu.component.scss']
+})
+export class MiniPageMenuComponent implements OnInit {
+
+  @Input()
+  page: Page;
+
+  constructor(private pageStructure: PageStructureService) { }
+
+  ngOnInit() {
+  }
+
+  public delete(): void {
+    this.pageStructure.removePage(this.page);
+  }
+
+  public copy(): void {
+    this.pageStructure.addToClipboard([this.page]);
+  }
+
+  public cut(): void {
+    this.pageStructure.cut([this.page]);
+  }
+
+}
