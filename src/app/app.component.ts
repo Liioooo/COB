0b0208237e3b7b92f7cd126fe3763816e51e1e04
Leo Preissegger <preissegger.leo@a1.net>
2019-03-19 16:52:09 +0100

    [TASK] designed left-sidebar, added font-awesome icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import {ApplicationRef, ChangeDetectorRef, Component, HostListener, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, HostListener, OnInit} from '@angular/core';
 import {PageViewGridService} from './services/page-view-grid/page-view-grid.service';
 import {PageStructureService} from './services/PageStructure/page-structure.service';
 import {ElectronService} from 'ngx-electron';
+import {MatIconRegistry} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,12 @@ export class AppComponent implements OnInit {
         public pageStructure: PageStructureService,
         public pageViewGrid: PageViewGridService,
         private electronService: ElectronService,
-        private changeRef: ChangeDetectorRef
-    ) {}
+        private changeRef: ChangeDetectorRef,
+        matIconRegistry: MatIconRegistry
+    ) {
+        matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+        matIconRegistry.registerFontClassAlias('fontawesomeRegular', 'far');
+    }
 
     ngOnInit(): void {
         this.electronService.ipcRenderer.on('menuClick', (event, response) => this.handleMenuClick(event, response));
