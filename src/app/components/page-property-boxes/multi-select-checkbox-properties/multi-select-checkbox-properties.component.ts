42786e6269c1236d8fa2fb9526b1875a9b7a45e0
Leo Preissegger <preissegger.leo@a1.net>
2019-04-30 17:40:30 +0200

    [TASK] multi select check box property box 1

--- src/app/components/page-property-boxes/multi-select-checkbox-properties/multi-select-checkbox-properties.component.ts
+++ src/app/components/page-property-boxes/multi-select-checkbox-properties/multi-select-checkbox-properties.component.ts
@@ -1,28 +1,52 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, SimpleChanges} from '@angular/core';
 import {Page} from "../../../models/page-interface";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-multi-select-checkbox-properties',
   templateUrl: './multi-select-checkbox-properties.component.html',
   styleUrls: ['./multi-select-checkbox-properties.component.scss']
 })
-export class MultiSelectCheckboxPropertiesComponent implements OnInit {
+export class MultiSelectCheckboxPropertiesComponent implements OnChanges, OnDestroy {
 
   @Input()
   public page: Page;
   public multiSelectCheckboxForm: FormGroup;
 
+  private subscription: Subscription;
+
   constructor(private formbuilder: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
     this.multiSelectCheckboxForm = this.formbuilder.group({
-      test: ["", []]
+      shortName: [this.page.shortName || ''],
+      title: [this.page.title || ''],
+      helpQuestion: [this.page.helpQuestion || ''],
+      helpTooltip: [this.page.helpTooltip || ''],
+      mandatory: [this.page.mandatory || ''],
+      handover: [this.page.handover || ''],
+      handoverText: [this.page.handoverText || '']
     });
 
-    this.multiSelectCheckboxForm.valueChanges.subscribe(console.log);
+    this.subscription = this.multiSelectCheckboxForm.valueChanges.subscribe(values => {
+      this.page.shortName = values.shortName;
+      this.page.title = values.title;
+      this.page.helpQuestion = values.helpQuestion;
+      this.page.helpTooltip = values.helpTooltip;
+      this.page.mandatory = values.mandatory !== '';
+      this.page.handover = values.handover !== '';
+      this.page.handoverText = values.handoverText;
+    });
   }
 
-
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
 }
--- src/app/components/right-sidebar/right-sidebar.component.ts
+++ src/app/components/right-sidebar/right-sidebar.component.ts
@@ -7,7 +7,7 @@ import {Page} from '../../models/page-interface';
   styleUrls: ['./right-sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RightSidebarComponent implements OnInit, OnChanges {
+export class RightSidebarComponent implements OnInit {
 
   @Input()
   selectedPage: Page;
@@ -18,6 +18,16 @@ export class RightSidebarComponent implements OnInit, OnChanges {
   ngOnInit() {
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  resetPage() {
+    this.selectedPage.templateType = 'none';
+    delete this.selectedPage.handover;
+    delete this.selectedPage.handoverText;
+    delete this.selectedPage.title;
+    delete this.selectedPage.mandatory;
+    delete this.selectedPage.helpTooltip;
+    delete this.selectedPage.helpQuestion;
+    delete this.selectedPage.shortName;
+    delete this.selectedPage.selectionControls;
+    delete this.selectedPage.sliderControl;
   }
 }
