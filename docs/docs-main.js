c7e1c6d567a4e27b6a2c70b9e2707f5d0acfc23b
Stefan Mikulik <6148@htl.rennweg.at>
2019-05-07 17:33:57 +0200

    [TASK] started creating the help file

--- docs/docs-main.js
+++ docs/docs-main.js
@@ -1,1 +1,24 @@
 import './docs-styles.scss';
+
+Array.from(document.getElementsByClassName("title")).forEach(function(element) {
+  element.addEventListener("click", toggleContent);
+});
+
+function toggleContent() {
+  console.log("got here");
+    let nextElement = next(this);
+    if (nextElement.style.display === 'none') {
+      nextElement.style.display = 'block';
+      console.log("block");
+    } else {
+      nextElement.style.display = 'none';
+      console.log("none");
+    }
+}
+
+function next(elem) {
+  do {
+    elem = elem.nextSibling;
+  } while (elem && elem.nodeType !== 1);
+  return elem;
+}
