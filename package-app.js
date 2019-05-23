ad1e9c2c89fad38cd39bfec4bf50da241fc24985
Leo Preissegger <preissegger.leo@gmail.com>
2019-05-23 11:12:49 +0200

    [FIX] fixed build process

--- package-app.js
+++ package-app.js
@@ -6,7 +6,6 @@ if (!platform || !arch) {
 }
 
 const packager = require('electron-packager');
-const rebuild = require('electron-rebuild').rebuild;
 
 const options = {
   'arch': arch,
@@ -25,12 +24,7 @@ const options = {
     'OriginalFilename': 'COB',
     'ProductName': 'COB',
     'InternalName': 'COB'
-  },
-  afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
-    rebuild({ buildPath, electronVersion, arch })
-      .then(() => callback())
-      .catch((error) => callback(error));
-  }],
+  }
 };
 
 packager(options)
